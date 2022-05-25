8b639fa8d4708120772d73922fe7772b859667b8
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-25 11:52:46 -0600

    added url to grab html from and set it up within an async function with try/catch

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url =
+  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries";
+
+async function run() {
+  try {
+    const { data } = await axios.get(url);
+    console.log(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+run();
