d89dbadcd3e71d97ef4113dd312d4a02b2035394
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-25 13:47:18 -0600

    added second url for web dev to array of urls and map used Promise.all to return Promise of mapped over urls

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url = "https://www.reddit.com/r/cscareerquestions/";
+const urls = [
+  "https://www.reddit.com/r/cscareerquestions/",
+  "https://www.reddit.com/r/webdev/",
+];
 
 async function run() {
-  try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const activity = $(
-      'div[data-testid="subreddit-sidebar"] div:first-child div:last-child div:nth-child(2) div:nth-child(2)'
-    ).get()[0];
-    const num = $(activity).text().replaceAll("Online", "").replaceAll(",", "");
-    console.log(num);
-  } catch (err) {
-    console.log(err.message);
-  }
+  const data = await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        const activity = $(
+          'div[data-testid="subreddit-sidebar"] div:first-child div:last-child div:nth-child(2) div:nth-child(2)'
+        ).get()[0];
+        const num = $(activity)
+          .text()
+          .replaceAll("Online", "")
+          .replaceAll(",", "");
+        console.log(num);
+        return { [url]: num };
+      } catch (err) {
+        return { [url]: null };
+        console.log(err.message);
+      }
+    })
+  );
+  console.log(data);
 }
 
 run();
